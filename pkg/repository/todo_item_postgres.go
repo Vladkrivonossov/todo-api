93594ea9b898e0ea3853e7787e1288f432b5260e
Vladkrivonossov <vladik374374@gmail.com>
2023-02-08 14:18:22 +0300

    fix migration scheme

--- pkg/repository/todo_item_postgres.go
+++ pkg/repository/todo_item_postgres.go
@@ -43,7 +43,7 @@ func (r *TodoItemPostgres) Create(listId int, item todo.TodoItem) (int, error) {
 
 func (r *TodoItemPostgres) GetAll(userId, listId int) ([]todo.TodoItem, error) {
   var items []todo.TodoItem
-  query := fmt.Sprintf("SELECT ti.id, ti.title, ti.description, ti.done FROM %s ti INNER JOIN %s li on li.item_id = ti.id INNER JOIN %s ul on ul.list_id = li.list_id WHERE li.list_id = $1 AND ul.user_id = $2", todoItemsTable, todoListsTable, usersListsTable)
+  query := fmt.Sprintf("SELECT ti.id, ti.title, ti.description, ti.done FROM %s ti INNER JOIN %s li on li.item_id = ti.id INNER JOIN %s ul on ul.list_id = li.list_id WHERE li.list_id = $1 AND ul.user_id = $2", todoItemsTable, listsItemsTable, usersListsTable)
 
   if err := r.db.Select(&items, query, listId, userId); err != nil {
     return nil, err
@@ -54,7 +54,7 @@ func (r *TodoItemPostgres) GetAll(userId, listId int) ([]todo.TodoItem, error) {
 
 func (r *TodoItemPostgres) GetById(userId, itemId int) (todo.TodoItem, error) {
   var item todo.TodoItem
-  query := fmt.Sprintf("SELECT ti.id, ti.title, ti.description, ti.done FROM %s ti INNER JOIN %s li on li.item_id = ti.id INNER JOIN %s ul on ul.list_id = li.list_id WHERE ti.id = $1 AND ul.user_id = $2", todoItemsTable, todoListsTable, usersListsTable)
+  query := fmt.Sprintf("SELECT ti.id, ti.title, ti.description, ti.done FROM %s ti INNER JOIN %s li on li.item_id = ti.id INNER JOIN %s ul on ul.list_id = li.list_id WHERE ti.id = $1 AND ul.user_id = $2", todoItemsTable, listsItemsTable, usersListsTable)
 
   if err := r.db.Get(&item, query, itemId, userId); err != nil {
     return item, err
@@ -76,27 +76,27 @@ func (r *TodoItemPostgres) Update(userId, itemId int, input todo.UpdateItemInput
   argId := 1
 
   if input.Title != nil {
-    setValues = append(setValues, fmt.Sprintf("title=%d", argId))
+    setValues = append(setValues, fmt.Sprintf("title=$%d", argId))
     args = append(args, *input.Title)
     argId++
   }
 
   if input.Description != nil {
-    setValues = append(setValues, fmt.Sprintf("description=%d", argId))
+    setValues = append(setValues, fmt.Sprintf("description=$%d", argId))
     args = append(args, *input.Description)
     argId++
   }
 
   if input.Done != nil {
-    setValues = append(setValues, fmt.Sprintf("done=%d", argId))
+    setValues = append(setValues, fmt.Sprintf("done=$%d", argId))
     args = append(args, *input.Done)
     argId++
   }
 
   setQuery := strings.Join(setValues, ", ")
 
-  query := fmt.Sprintf("UPDATE %s ti SET %s FROM %s li, %s ul WHERE ti.id = li.item_id AND li.list_id = ul.list_id AND ul.user_id = $%d AND ti.id = $%d", todoItemsTable, setQuery, listsItemsTable, usersListsTable, argId, argId+1)
-
+  query := fmt.Sprintf("UPDATE %s ti SET %s FROM %s li, %s ul WHERE ti.id = li.item_id AND li.list_id = ul.list_id AND ul.user_id = $%d AND ti.id = $%d",
+todoItemsTable, setQuery, listsItemsTable, usersListsTable, argId, argId+1)
   args = append(args, userId, itemId)
 
   _, err := r.db.Exec(query, args...)
